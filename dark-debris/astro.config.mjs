6f81240367cafc259f495e14dc18de1cb3ca52f3
nguyenq9 <nguyenq9@wwu.edu>
2024-02-26 18:01:42 -0800

    Thai: add a change to config to change build format to file

--- dark-debris/astro.config.mjs
+++ dark-debris/astro.config.mjs
@@ -4,5 +4,9 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  build: {
+    // Example: Generate `page.html` instead of `page/index.html` during build.
+    format: 'file'
+  }
 });
