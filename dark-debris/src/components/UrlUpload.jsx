bd98347721d63dfc451c707dd7ffd6a3e568d982
alex kefer <69139463+alexkefer@users.noreply.github.com>
2024-02-29 23:55:44 -0800

    Developed two versions of the same layout, one in astro, one in react, just for kicks, implemented live image viewing, currently shows a broken image link if the link is incorrect, looking for alternative, also switched local tasks to switcher

---
+++ dark-debris/src/components/UrlUpload.jsx
@@ -0,0 +1,42 @@
+// React Integration of UrlUpload component
+import React, { useState } from 'react';
+
+export default function UrlUpload({key, endpoint}) {
+	const [url, setUrl] = useState('');
+
+	const handleChange = async (event) => {
+		setUrl(event.target.value);
+	};
+
+	return (
+		<astro-upload data-endpoint={endpoint} data-key={key}>
+			<div className="wwu-card horizontal dark-blue-bg">
+				<div className="body">
+					<form style={{marginTop: "20px", marginBottom: "20px"}}>
+						<label htmlFor="url-upload">URL</label>
+						<input
+							id="url-upload"
+							type="url"
+							placeholder="Enter a URL"
+							style={{width: "100%", lineHeight: "1.5em"}}
+							onInput={handleChange}
+							required
+						/>
+						<label htmlFor="prompt">Prompt <i>[Optional]</i></label>
+						<textarea
+							id="prompt"
+							placeholder="Enter a prompt"
+							rows="5"
+							wrap="hard"
+							style={{width: "100%"}}
+						/>
+					</form>
+					<button id="evaluate-image" style={{ marginBottom: '10px'}}>Evaluate Image</button>
+				</div>
+				<div style={{objectFit:'contain'}}>
+					{url && <img src={url} alt="Image failed to load. Did you submit the correct link?" style={{objectFit: 'contain', margin: '10px', width: '95%'}}/>}
+						</div>
+						</div>
+						</astro-upload>
+						);
+					}
