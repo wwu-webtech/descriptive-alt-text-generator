c661ba47f98fb279fe429a38c49f1a39cb0bb111
alex kefer <69139463+alexkefer@users.noreply.github.com>
2024-03-02 21:52:50 -0800

    Built URLAzure call, ran into issue using CORS, sent an email regarding it.

--- dark-debris/src/components/UrlUpload.jsx
+++ dark-debris/src/components/UrlUpload.jsx
@@ -1,15 +1,43 @@
 // React Integration of UrlUpload component
 import React, { useState } from 'react';
+import {AzureURLRequest} from "../api/AzureURLRequest.jsx";
+import { handleAzureCall } from "../js/API.js";
 
-export default function UrlUpload({key, endpoint}) {
+export default function UrlUpload({api_key, endpoint}) {
 	const [url, setUrl] = useState('');
+	const [blob, setBlob] = useState(null);
+
+	const callAzure = async () => {
+		try {
+			const response = await fetch("https://cors-anywhere.herokuapp.com/" + url, {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+					"Accept": "application/json",
+				},
+			});
+			if (!response.ok) {
+				throw new Error(`Failed to fetch image: ${response.statusText}`);
+			}
+			const blobData = await response.blob();
+			setBlob(blobData);
+			handleAzureCall(api_key, endpoint, blobData)
+		} catch (error) {
+			console.error("Error fetching image:", error);
+		}
+	};
 
 	const handleChange = async (event) => {
 		setUrl(event.target.value);
 	};
+	
+	const evaluateImage = async () => {
+		const prompt = document.getElementById("prompt").value;
+		AzureURLRequest({url, prompt, api_key, endpoint});
+	}
 
 	return (
-		<astro-upload data-endpoint={endpoint} data-key={key}>
+		<astro-upload data-endpoint={endpoint} data-key={api_key}>
 			<div className="wwu-card horizontal dark-blue-bg">
 				<div className="body">
 					<form style={{marginTop: "20px", marginBottom: "20px"}}>
@@ -31,12 +59,12 @@ export default function UrlUpload({key, endpoint}) {
 							style={{width: "100%"}}
 						/>
 					</form>
-					<button id="evaluate-image" style={{ marginBottom: '10px'}}>Evaluate Image</button>
+					<button onClick={callAzure} id="evaluate-image" style={{ marginBottom: '10px'}}>Evaluate Image</button>
 				</div>
 				<div style={{objectFit:'contain'}}>
 					{url && <img src={url} alt="Image failed to load. Did you submit the correct link?" style={{objectFit: 'contain', margin: '10px', width: '95%'}}/>}
-						</div>
-						</div>
-						</astro-upload>
-						);
-					}
+				</div>
+			</div>
+		</astro-upload>
+	);
+}
