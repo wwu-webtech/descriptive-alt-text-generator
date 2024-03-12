cff23ac87158a185c00587215732d876aafc6e14
nguyenq9 <nguyenq9@wwu.edu>
2024-03-11 20:14:22 -0700

    Thai: adding waiting for refine results and moved gif to public dir

--- dark-debris/astro.config.mjs
+++ dark-debris/astro.config.mjs
@@ -1,12 +1,13 @@
+// astro.config.js
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   build: {
-    // Example: Generate `page.html` instead of `page/index.html` during build.
     format: 'file'
-  }
+  },
+  images: {
+    extensions: ['jpg', 'jpeg', 'png', 'gif'],
+  },
 });
--- dark-debris/src/js/RefineHelper.js
+++ dark-debris/src/js/RefineHelper.js
@@ -1,7 +1,17 @@
 import { handleGeminiRefineResults } from "./API";
 
 const button = document.getElementById("refine-gemini-button");
-button.addEventListener("click", () => {
+button.addEventListener("click", async () => {
+  const loading = document.getElementById("evaluate-loading");
+  loading.showModal();
   console.log("Refining...");
-  handleGeminiRefineResults();
+
+  try {
+    await handleGeminiRefineResults();
+  } catch (error) {
+    console.error(error);
+  } finally {
+    loading.close();
+  }
+
 });
