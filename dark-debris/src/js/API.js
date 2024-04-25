7f5c3b36b3f1a6989860b6324f2e55871a69e5c2
nguyenq9 <nguyenq9@wwu.edu>
2024-04-24 20:33:55 -0700

    Thai: got rid of Azure, working on modal

--- dark-debris/src/js/API.js
+++ dark-debris/src/js/API.js
@@ -1,26 +1,27 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import OpenAI from "openai";
+import { showErrorDialog } from "./ModalHelper";
 
 var limit_response = document.getElementById("limit-response").checked
 
-const showErrorDialog = (msg) => {
-  const dialog = document.getElementById("evaluate-error")
-  const message = document.getElementById("evaluate-error-message")
-  message.textContent = msg
-  dialog.showModal();
-}
+// const showErrorDialog = (msg) => {
+//   const dialog = document.getElementById("evaluate-error")
+//   const message = document.getElementById("evaluate-error-message")
+//   message.textContent = msg
+//   dialog.showModal();
+// }
 
 const handleAzureCall = async () => {
   const canvas = document.getElementById("canvas");
   const headers = {
     "Content-Type": "application/octet-stream",
-    "Ocp-Apim-Subscription-Key": import.meta.env.PUBLIC_MSFT_COGNITIVE_AI,
+    "Ocp-Apim-Subscription-Key": import.meta.env.PUBLIC_MSFT_COGNITIVE_A,
   };
 
 
   canvas.toBlob((blob) => {
     if (!blob) {
-      showErrorDialog("No image to evaluate")
+      showErrorDialog("No image to evaluate. (Azure)")
       console.error("Error: No image blob available");
       return;
     }
@@ -41,7 +42,7 @@ const handleAzureCall = async () => {
 
       })
       .catch((error) => {
-        showErrorDialog("Azure API Error.")
+        showErrorDialog(`Azure API Error. Error: ${error}`)
         console.error("Error: ", error);
       });
   });
@@ -52,7 +53,7 @@ const handleGeminiCall = async () => {
 
   const dataURL = canvas.toDataURL("image/jpeg", 0.5);
   if (dataURL !== "data:,") {
-    const genAI = new GoogleGenerativeAI(import.meta.env.PUBLIC_GEMINI_KEY);
+    const genAI = new GoogleGenerativeAI(import.meta.env.PUBLIC_GEMINI_KE);
     const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
     var limit_response = document.getElementById("limit-response").checked
     
@@ -86,11 +87,11 @@ const handleGeminiCall = async () => {
       document.getElementById("gemini-area").value = text;
 
     } catch (error) {
-      showErrorDialog("Gemini API Error.")
+      showErrorDialog(`Oops! There was an error with the Gemini API. Please try again later or contact support for assistance.\n Error Message: ${error.message}`)
       console.error("Error during API request:", error.message);
     }
   } else {
-    showErrorDialog("No image to evaluate.")
+    showErrorDialog("No image to evaluate. (Gemini)")
     console.error("There is no image to evaluate!");
   }
 };
